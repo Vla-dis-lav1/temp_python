ff2a4311c95d3ea60d4f6f3239a16bf5cec694df
Vla-dis-lav1 <168162289+Vla-dis-lav1@users.noreply.github.com>
2024-04-27 09:50:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,247 +1,103 @@
 import tokens_list
 
 
-class Base:
+def is_char_list_contain_current_char(current_char, list_to_check):
+    for char in list_to_check:
+        if char == current_char:
+            return True
+    return False
+
+
+def get_char_position_in_list(current_char, list_to_check):
+    counter = 0
+    for elem in list_to_check:
+        if elem == current_char:
+            return counter
+        else:
+            counter += 1
+    return counter - 1
+
+
+class Token:
     file = None
     tokens_name = []
     tokens_value = []
-    tokens1 = []
-    tokens2 = []
-    tokens3 = []
-    tokens4 = []
-    counter = 0
+    tokens_name_result = []
+    tokens_value_result = []
+    tokens_name_and_value_result = []
+    tokens_lines = []
 
     def __init__(self, file_name, modifier):
         self.file = open(file_name, modifier)
         self.tokens_name = tokens_list.tokens_name
         self.tokens_value = tokens_list.tokens_value
 
-    def condition(self, char):
-        for i in self.tokens_value:
-            if i == char:
-                return True
-        return False
-
-    def work(self):
+    def split_tokens(self):
         string = ""
         for line in self.file:
             for current_char in line:
-                if self.condition(current_char):
-                    if len(string) > 0:
-                        self.tokens1.append(string)
+                if is_char_list_contain_current_char(current_char, self.tokens_value):
+                    if string != "":
+                        self.tokens_name_result.append(string)
                         string = ""
-                    self.tokens1.append(current_char)
-                if not self.condition(current_char):
-                    string = string + current_char
+                        self.tokens_name_result.append(current_char)
+                    elif string == "":
+                        self.tokens_name_result.append(current_char)
+                elif not is_char_list_contain_current_char(current_char, self.tokens_value):
+                    string += current_char
         return self
 
-    def work2(self):
-        for i in self.tokens1:
-            self.counter = 0
-            for i2 in self.tokens_value:
-                if i == i2:
-                    temp = self.tokens_name[self.counter]
-                    self.tokens2.append(temp)
-                self.counter += 1
-            if not self.condition(i):
-                if len(i) > 1:
-                    self.tokens2.append("word")
+    def add_names(self):
+        for i in self.tokens_name_result:
+            if is_char_list_contain_current_char(i, self.tokens_value):
+                counter = get_char_position_in_list(i, self.tokens_value)
+                self.tokens_value_result.append(self.tokens_name[counter])
+            elif not is_char_list_contain_current_char(i, self.tokens_value):
                 if len(i) == 1:
-                    self.tokens2.append("character")
+                    self.tokens_value_result.append('character')
+                elif len(i) > 1:
+                    self.tokens_value_result.append('word')
         return self
 
-    def union_name_and_value(self):
-        self.counter = 0
-        while self.counter < len(self.tokens2) and self.counter < len(self.tokens1):
-            temp_list = [self.tokens2[self.counter], self.tokens1[self.counter]]
-            self.tokens3.append(temp_list)
-            self.counter += 1
-        self.tokens4 = self.tokens3
+    def union_names_and_values(self):
+        counter = 0
+        for i in self.tokens_value_result:
+            temp_list = [self.tokens_value_result[counter], self.tokens_name_result[counter]]
+            self.tokens_name_and_value_result.append(temp_list)
+            counter += 1
+        return self
+
+    def split_lines(self):
+        temp_list = []
+        for i in self.tokens_name_and_value_result:
+            if i[0] == "new line":
+                temp_list.append(i)
+                self.tokens_lines.append(temp_list)
+                temp_list = []
+            else:
+                temp_list.append(i)
+
+    def print_tokens(self):
+        print(self.tokens_name_result)
+        return self
+
+    def print_tokens2(self):
+        print(self.tokens_value_result)
+        return self
+
+    def print_tokens3(self):
+        print(self.tokens_name_and_value_result)
+        return self
+
+    def print_lines(self):
+        for line in self.tokens_lines:
+            print(line)
         return self
 
     def __del__(self):
         self.file.close()
 
 
-base = Base("D:\\asd\\char_array.c", "r")
-base.work().work2().union_name_and_value()
-
-
-class Token:
-    result_list = []
-    length = 0
-    pointer = 0
-    pair = []
-    name = ""
-    value = ""
-    condition = False
-    temp_list = []
-    result_list2 = []
-    lines_length = 0
-    lines_pointer = 0
-    lines_condition = False
-    lines_current_line = []
-
-    def __init__(self, list1, start_pointer=0):
-        self.result_list = list1
-        self.length = len(self.result_list)
-        self.pointer = start_pointer
-        self.update_pair_name_value()
-        self.switch()
-
-    def switch(self):
-        if not self.condition:
-            self.condition = True
-        elif self.condition:
-            self.condition = False
-        return self
-
-    def update_pair_name_value(self):
-        self.pair = self.get_current_pair()
-        self.name = self.get_current_name()
-        self.value = self.get_current_value()
-        return self
-
-    def next(self, count=1):
-        if self.pointer >= self.length - 1:
-            self.pointer = self.length - 1
-            self.switch()
-        else:
-            self.pointer += count
-        self.update_pair_name_value()
-        return self
-
-    def back(self, count=1):
-        if self.pointer <= 0:
-            self.pointer = 0
-        else:
-            self.pointer -= count
-        self.update_pair_name_value()
-        return self
-
-    def get_current_pair(self):
-        return self.result_list[self.pointer]
-
-    def get_current_name(self):
-        return self.get_current_pair()[0]
-
-    def get_current_value(self):
-        return self.get_current_pair()[1]
-
-    def print_pair(self):
-        print(self.pair)
-        return self
-
-    def print_name(self):
-        print(self.name)
-        return self
-
-    def print_value(self):
-        print(self.value)
-        return self
-
-    def print_temp_cycle(self):
-        print(self.condition)
-        return self
-
-    def print_temp_list(self):
-        print(self.temp_list)
-        return self
-
-    def switch_results_lists(self):
-        self.result_list = self.result_list2
-        self.result_list2 = []
-        return self
-
-    def set_default(self):
-        self.pointer = 0
-        if not self.condition:
-            self.switch()
-        if len(self.temp_list) > 0:
-            self.temp_list = []
-        return self
-
-    def print_lines(self, value=True):
-        if not value:
-            return self
-        for i in self.result_list:
-            print(i)
-        return self
-
-    def temp_list_append(self, value_for_append):
-        self.temp_list.append(value_for_append)
-        return self
-
-    def result_list2_append(self, value_for_append):
-        self.result_list2.append(value_for_append)
-        return self
-
-    def set_temp_list_empty(self):
-        self.temp_list = []
-        return self
-
-    def split_lines(self):
-        while self.condition:
-            if self.name == "new line":
-                self.temp_list_append(
-                    self.get_current_pair()
-                ).result_list2_append(
-                    self.temp_list
-                ).set_temp_list_empty()
-            else:
-                self.temp_list_append(self.get_current_pair())
-            self.next()
-        self.lines_set_default()
-        return self
-
-    def lines_set_default(self):
-        self.lines_length = len(self.result_list)
-        self.lines_pointer = 0
-        self.lines_condition = True
-        self.lines_update_current()
-        return self
-
-    def lines_set_current(self):
-        self.lines_current_line = self.result_list[self.lines_pointer]
-        return self
-
-    def lines_get_current(self):
-        return self.lines_current_line
-
-    def lines_update_current(self):
-        self.lines_set_current()
-        return self
-
-    def lines_switch(self):
-        if not self.lines_condition:
-            self.lines_condition = True
-        elif self.lines_condition:
-            self.lines_condition = False
-        return self
-
-    def lines_next(self, count=1):
-        self.lines_length = len(self.result_list)
-        if self.lines_pointer >= self.lines_length - 1:
-            self.lines_pointer = self.lines_length - 1
-            self.lines_switch()
-        else:
-            self.lines_pointer += count
-        self.lines_update_current()
-        return self
-
-    def lines_back(self, count=1):
-        if self.lines_pointer <= 0:
-            self.lines_pointer = 0
-        else:
-            self.lines_pointer -= count
-        self.lines_update_current()
-        return self
-
-    def lines_print_current_line(self):
-        print(self.lines_get_current())
-        return self
-
-
-# token = Token(base.tokens4)
-
-# token.split_lines().switch_results_lists().print_lines(False).set_default()
+token = Token("D:\\asd\\char_array.c", "r")
+token.split_tokens().add_names().union_names_and_values().split_lines()
+token.print_lines()
